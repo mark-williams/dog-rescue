77ce7c6c77f6170d6cf9444b83cef59d4f945825
mark-williams <markwill@hotmail.co.uk>
2016-11-29 10:00:35 +0000

    Added some hard-coded state (will reduxify once components mapped out)

--- src/components/DogsMaintenance.js
+++ src/components/DogsMaintenance.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-const DogsMaintenance = () => (
-  <div>
-    <h4>Dog Maintenance</h4>
-    <ul>
-      <li>Chaz</li>
-      <li>Bert</li>
-      <li>Daisy</li>
-      <li>Luca</li>
-    </ul>
-  </div>
-);
+const initialState = {
+  dogs: [
+    { id: 1000, name: 'Chaz', acquired: new Date(2011, 1, 1), sex: 'B' },
+    { id: 1001, name: 'Bert', acquired: new Date(2010, 1, 1), sex: 'D' },
+    { id: 1002, name: 'Daisy', acquired: new Date(2015, 7, 1), sex: 'B' },
+    { id: 1003, name: 'Luca', acquired: new Date(2014, 3, 6), sex: 'D' },
+  ],
+};
+
+class DogsMaintenance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Dog Maintenance</h4>
+        <ul>
+          { this.state.dogs.map(dog => (<li>{dog.name}</li>))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default DogsMaintenance;
