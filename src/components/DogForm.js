908131e1f3dc0fba4885aa3f49b4f3c8a0a7da1c
mark-williams <markwill@hotmail.co.uk>
2016-12-08 17:49:18 +0000

    Dog sex now captured by radio buttons

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -33,6 +33,13 @@ class DogForm extends React.Component {
         </div>
         <div className="row">
           <div className="col s6 left-align">
+            <label htmlFor="breed">Sex</label>
+            <input type="radio" id="sex-radio-dog" name="sex" value="Dog" checked={this.state.item.sex === 'Dog'} onChange={this.onChange} /><label htmlFor="sex-radio-dog">Dog</label>
+            <input type="radio" id="sex-radio-bitch" name="sex" value="Bitch" checked={this.state.item.sex === 'Bitch'} onChange={this.onChange} /><label htmlFor="sex-radio-bitch">Bitch</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s6 left-align">
             <button className="waves-effect waves-light btn" onClick={() => this.props.onSave(this.state.item)}>Save</button>
           </div>
         </div>
@@ -47,4 +54,3 @@ DogForm.propTypes = {
 };
 
 export default DogForm;
-
