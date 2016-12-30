67f5d747797dc50aa0e4bb7e03b3500f968b7450
mark-williams <markwill@hotmail.co.uk>
2016-12-30 09:53:46 +0000

    Recatored tests

--- src/components/Dog.spec.js
+++ src/components/Dog.spec.js
@@ -3,11 +3,15 @@ import TestUtils from 'react-addons-test-utils';
 import Dog from './Dog';
 
 describe('Dog tests', () => {
-  let props = {};
+  const testProps = {
+    name: 'Fred',
+    sex: 'dog',
+    breed: 'Labrador',
+  };
 
   const setup = (props) => {
     const renderer = TestUtils.createRenderer();
-    renderer.render(<Dog name={props.name} sex={props.sex} breed={props.breed} />);
+    renderer.render(<Dog {...props} />);
     const output = renderer.getRenderOutput();
 
     return {
@@ -16,35 +20,28 @@ describe('Dog tests', () => {
     };
   };
 
-  beforeEach(() => {
-    props = {
-      name: 'Fred',
-      sex: 'dog',
-      breed: 'Labrador'
-    };
-  });
 
   it('renders a table row', () => {
-    const { output } = setup(props);
+    const { output } = setup(testProps);
     expect(output.type).toBe('tr');
   });
 
   it('renders name', () => {
-    const { output } = setup(props);
+    const { output } = setup(testProps);
     expect(output.props.children[1].type).toBe('td');
-    expect(output.props.children[1].props.children.props.children).toBe(props.name);
+    expect(output.props.children[1].props.children.props.children).toBe(testProps.name);
   });
 
   it('renders sex', () => {
-    const { output } = setup(props);
+    const { output } = setup(testProps);
     expect(output.props.children[2].type).toBe('td');
-    expect(output.props.children[2].props.children).toBe(props.sex);
+    expect(output.props.children[2].props.children).toBe(testProps.sex);
   });
 
   it('renders breed', () => {
-    const { output } = setup(props);
+    const { output } = setup(testProps);
     expect(output.props.children[3].type).toBe('td');
-    expect(output.props.children[3].props.children).toBe(props.breed);
+    expect(output.props.children[3].props.children).toBe(testProps.breed);
   });
 
 });
